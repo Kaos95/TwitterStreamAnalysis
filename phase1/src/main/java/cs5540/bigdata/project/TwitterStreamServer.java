9a79035a0acd07329e7ac9f4f3b15d2907514a8a
Hayden McParlane <hayden.mcparlane@gmail.com>
2015-11-27 18:43:55 -0600

    Delete TwitterStreamServer.java
    Outdated version of the project. No longer needed. Doesn't work.

--- phase1/src/main/java/cs5540/bigdata/project/TwitterStreamServer.java
+++
@@ -1,9 +0,0 @@
-package cs5540.bigdata.project;
-
-public class TwitterStreamServer {
-	public static void main(String[] args){
-		final TwitterStreamConsumer streamConsumer = 
-			new TwitterStreamConsumer();
-		streamConsumer.start();
-	}
-}
